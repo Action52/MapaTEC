8bcc60b655ae0b2cdb5c843c4d835b72cc6d206d
Luis Alfredo León Villapún <leonvillapun@gmail.com>
2017-07-10 11:22:34 -0500

    Se sigue mejorando la barra de navegación

--- MapaTEC/resources/views/includes/headerLogged.blade.php
+++ MapaTEC/resources/views/includes/headerLogged.blade.php
@@ -1,40 +1,69 @@
 <div class ="row header-mapatec">
   <div class ="col-md-2">
     <!--Aqui va el logo-->
-    <a href="{{ URL('home') }}">{{ Html::image('../img/logo.png', 'Menú Principal', array('height' => '75', 'width' => '75')) }}
-    {{ Html::image('../img/itesm.png', 'Menú Principal', array('height' => '75', 'width' => '75')) }}</a>
+    <a href="{{ URL('home') }}">
+    {{ Html::image('../img/itesm.png', 'Menú Principal', array('height' => '52', 'width' => '52')) }}</a>
   </div>
   <div class ="col-md-5">
-
+    <!--div class ="row">
+      <div class ="col-sm-3">
+        Mapa Tec
+      </div>
+      <div class ="col-sm-3">
+        ¿Quiénes somos?
+      </div>
+      <div class ="col-sm-3">
+        <a href="">Registro</a>
+      </div>
+    </div!-->
   </div>
   <div class ="col-md-5">
     <div class ="row">
-      <div class ="col-sm-4">
-        <a href="crudproyectos/create">{{ Html::image('../img/newproject.png', 'Nuevo Proyecto', array('height' => '25', 'width' => '25')) }}</a>
-        <a href ="{{ URL::to('user/' . Auth::user()->id . '/edit') }}">{{ Html::image('../img/editaccount.png', 'Editar cuenta', array('height' => '25', 'width' => '25')) }}</a>
+      <div class ="col-sm-3">
+
       </div>
+      <div class ="col-sm-6">
 
+        <div class ="menu-wrap">
+          <nav class ="menu">
+            <ul class ="clearfix">
+              <li>
+                {{ Auth::user()->name }}
 
+              @if(Auth::user()->has_profile_pic == 1)
+                <a href="{{ URL::to('crudproyectos') }}">{{ Html::image('../img/profilePics/' . Auth::user()->id . '.png' , 'Perfil', array('height' => '50', 'width' => '50', 'class' => 'profile-pic')) }}</a>
+              @else
+                <a href="{{ URL::to('crudproyectos') }}">{{ Html::image('../img/profilePics/default.png' , 'Perfil', array('height' => '50', 'width' => '50')) }}</a>
+              @endif
 
+                <span class="arrow">&#9660;</span>
+                <ul class="sub-menu">
+                  <li>
+                    <a href="crudproyectos/create">Crear proyecto</a>
+                  </li>
+                  <li>
+                    <a href ="{{ URL::to('user/' . Auth::user()->id . '/edit') }}">Editar cuenta</a>
+                  </li>
+                  <li>
+                    <a href="{{ route('logout') }}"
+                        onclick="event.preventDefault();
+                                 document.getElementById('logout-form').submit();">
+                        Cerrar sesión
+                    </a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        {{ csrf_field() }}
+                    </form>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </nav>
+        </div>
 
-      <!--Aqui va el form de logout-->
-      <div class="col-sm-1">
-        <a href="{{ route('logout') }}"
-            onclick="event.preventDefault();
-                     document.getElementById('logout-form').submit();">
-            {{ Html::image('../img/logout.png', 'Cerrar sesión', array('height' => '25', 'width' => '25')) }}
-        </a>
       </div>
-      <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-          {{ csrf_field() }}
-      </form>
-    </div>
-    <div class ="row">
-      @if(Auth::user()->has_profile_pic == 1)
-        <a href="{{ URL::to('crudproyectos') }}">{{ Html::image('../img/profilePics/' . Auth::user()->id . '.png' , 'Perfil', array('height' => '50', 'width' => '50', 'class' => 'profile-pic')) }}</a>
-      @else
-        <a href="{{ URL::to('crudproyectos') }}">{{ Html::image('../img/profilePics/default.png' , 'Perfil', array('height' => '50', 'width' => '50')) }}</a>
-      @endif
+      <div class ="col-sm-1">
+        <a href="{{ URL('home') }}">{{ Html::image('../img/logo.png', 'Menú Principal', array('height' => '52', 'width' => '52')) }}</a>
+      </div>
     </div>
   </div>
 
